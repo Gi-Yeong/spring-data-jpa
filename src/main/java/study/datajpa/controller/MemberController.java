d42fc00f1d71936c14452cb3acf5946213342566
kiyoung85 <kiyoung85@gmail.com>
2020-10-14 22:30:57 +0900

    Web 확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,14 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -27,8 +32,20 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        int pageNumber = pageable.getPageSize();
+        System.out.println("pageNumber = " + pageNumber);
+        // 요청 URL ex) http://localhost/members?page=0&size=3&sort=id,desc&sort=username,desc
+        Page<Member> page = memberRepository.findAll(pageable);
+//        return page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+        return page.map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
